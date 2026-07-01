abf771a9ad7a2d678cafce84416ae195a1f08680
KASTEN <bashlaevs@ya.ru>
2026-07-01 10:33:59 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "2b56633b413d89f9aeb6f89b42cccad7",
+"flutter_bootstrap.js": "5ee1a975f6cb3cdaa137bb1c1cb22912",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
